750e14749b4dd79e8dfa2cfa965e273a21453c2d
Sofi Dobriak <sofi.dobriak@gmail.com>
2024-11-27 16:46:43 +0200

    Delete everything

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,1 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, 0)}`;
-}
-
-const input = document.querySelector('input');
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
+'use strict';
